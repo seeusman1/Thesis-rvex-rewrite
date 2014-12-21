0fcc80c4816f7658eb89615d3db63d31177aa8a8
Jeroen van Straten <jeroen.van.straten@gmail.com>
2014-12-21 13:35:36 +0100

    Added timeout methods which should work on linux and cygwin...

---
+++ debug-interface/src/rvsrv/timeout.c
@@ -0,0 +1,74 @@
+/* Debug interface for standalone r-VEX processor
+ * 
+ * Copyright (C) 2008-2014 by TU Delft.
+ * All Rights Reserved.
+ * 
+ * THIS IS A LEGAL DOCUMENT, BY USING r-VEX,
+ * YOU ARE AGREEING TO THESE TERMS AND CONDITIONS.
+ * 
+ * No portion of this work may be used by any commercial entity, or for any
+ * commercial purpose, without the prior, written permission of TU Delft.
+ * Nonprofit and noncommercial use is permitted as described below.
+ * 
+ * 1. r-VEX is provided AS IS, with no warranty of any kind, express
+ * or implied. The user of the code accepts full responsibility for the
+ * application of the code and the use of any results.
+ * 
+ * 2. Nonprofit and noncommercial use is encouraged. r-VEX may be
+ * downloaded, compiled, synthesized, copied, and modified solely for nonprofit,
+ * educational, noncommercial research, and noncommercial scholarship
+ * purposes provided that this notice in its entirety accompanies all copies.
+ * Copies of the modified software can be delivered to persons who use it
+ * solely for nonprofit, educational, noncommercial research, and
+ * noncommercial scholarship purposes provided that this notice in its
+ * entirety accompanies all copies.
+ * 
+ * 3. ALL COMMERCIAL USE, AND ALL USE BY FOR PROFIT ENTITIES, IS EXPRESSLY
+ * PROHIBITED WITHOUT A LICENSE FROM TU Delft (J.S.S.M.Wong@tudelft.nl).
+ * 
+ * 4. No nonprofit user may place any restrictions on the use of this software,
+ * including as modified by the user, by any other authorized user.
+ * 
+ * 5. Noncommercial and nonprofit users may distribute copies of r-VEX
+ * in compiled or binary form as set forth in Section 2, provided that
+ * either: (A) it is accompanied by the corresponding machine-readable source
+ * code, or (B) it is accompanied by a written offer, with no time limit, to
+ * give anyone a machine-readable copy of the corresponding source code in
+ * return for reimbursement of the cost of distribution. This written offer
+ * must permit verbatim duplication by anyone, or (C) it is distributed by
+ * someone who received only the executable form, and is accompanied by a
+ * copy of the written offer of source code.
+ * 
+ * 6. r-VEX was developed by Stephan Wong, Thijs van As, Fakhar Anjam,
+ * Roel Seedorf, Anthony Brandon, Jeroen van Straten. r-VEX is currently
+ * maintained by TU Delft (J.S.S.M.Wong@tudelft.nl).
+ * 
+ * Copyright (C) 2008-2014 by TU Delft.
+ */
+
+#include <sys/time.h>
+#include "timeout.h"
+
+// Because of the incredible amount of platform dependence related to C
+// functions returning time accurately, I decided to put these in their own
+// file. If compilation fails on timeout.c: good luck.
+
+/**
+ * Starts a timeout timer, identified by the result value.
+ */
+int startTimeout(void) {
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+
+/**
+ * Returns whether the specified timeout has timed out. start should be set to
+ * the value returned by startTimeout(), timeout should be set to the desired
+ * timeout in microseconds.
+ */
+int isTimedOut(int start, int timeout) {
+  int now = startTimeout();
+  int dif = now - start;
+  return dif > timeout;
+}
