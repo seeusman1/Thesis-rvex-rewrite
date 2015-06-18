ab36f75b7e8aabdcd44256560e49e9821d44007d
Anthony Brandon <a.a.c.brandon@tudelft.nl>
2015-06-18 14:17:38 +0200

    Enable tracing of cache performance in ml605-grlib platform.

--- lib/rvex/system/rvsys_grlib.vhd
+++ lib/rvex/system/rvsys_grlib.vhd
@@ -226,7 +226,8 @@ architecture Behavioral of rvsys_grlib is
   signal cache2rv_blockReconfig : std_logic_vector(2**CFG.core.numLaneGroupsLog2-1 downto 0);
   signal cache2rv_stallIn       : std_logic_vector(2**CFG.core.numLaneGroupsLog2-1 downto 0);
   signal rv2cache_stallOut      : std_logic_vector(2**CFG.core.numLaneGroupsLog2-1 downto 0);
-  
+  signal cache2rv_status        : rvex_cacheStatus_array(2**CFG.core.numLaneGroupsLog2-1 downto 0);
+ 
   -- Instruction cache interface signals.
   signal rv2icache_PCs          : rvex_address_array(2**CFG.core.numLaneGroupsLog2-1 downto 0);
   signal rv2icache_fetch        : std_logic_vector(2**CFG.core.numLaneGroupsLog2-1 downto 0);
@@ -347,7 +348,8 @@ begin -- architecture
         mem2rv_blockReconfig      => cache2rv_blockReconfig,
         mem2rv_stallIn            => cache2rv_stallIn,
         rv2mem_stallOut           => rv2cache_stallOut,
-        
+        mem2rv_cacheStatus        => cache2rv_status,
+       
         -- Instruction memory interface.
         rv2imem_PCs               => rv2icache_PCs,
         rv2imem_fetch             => rv2icache_fetch,
@@ -588,6 +590,7 @@ begin -- architecture
       cache2rv_blockReconfig    => cache2rv_blockReconfig,
       cache2rv_stallIn          => cache2rv_stallIn,
       rv2cache_stallOut         => rv2cache_stallOut,
+      cache2rv_status           => cache2rv_status,
       
       -- Core instruction memory interface.
       rv2icache_PCs             => rv2icache_PCs,
