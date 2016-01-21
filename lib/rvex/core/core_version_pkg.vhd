9fd79932a83d611372501cb7484e9e6e194a9f36
Jeroen van Straten <jeroen.van.straten@gmail.com>
2016-01-21 20:17:09 +0100

    grlib makefile wasn't loading from the patch file properly because of some dos2unix thing, also updated core version tag.

--- lib/rvex/core/core_version_pkg.vhd
+++ lib/rvex/core/core_version_pkg.vhd
@@ -56,8 +56,8 @@ package core_version_pkg is
   
   -- The value below is the version tag for the core, which is automatically
   -- generated based on the MD5 hash of the contents of the other source files.
-  -- ASCII tag = 2S2Zkvr
-  constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"3253325A6B7672";
+  -- ASCII tag = pwPV27g
+  constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"70775056323767";
   
 end core_version_pkg;
 
