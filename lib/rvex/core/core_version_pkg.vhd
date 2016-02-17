55468f49fc8a4a7bb3cb23202111269817f3ad60
Jeroen van Straten <jeroen.van.straten@gmail.com>
2016-02-17 13:17:15 +0100

    Lowered ml605-grlib frequency to 30MHz because the synthesis tools are having trouble.
    37.5 MHz probably still works just fine, but not with all the debugging features
    and performance counters enabled.

--- lib/rvex/core/core_version_pkg.vhd
+++ lib/rvex/core/core_version_pkg.vhd
@@ -56,8 +56,8 @@ package core_version_pkg is
 
 -- The value below is the version tag for the core, which is automatically
 -- generated based on the MD5 hash of the contents of the other source files.
--- ASCII tag = zD7Ngku
-constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"7A44374E676B75";
+-- ASCII tag = eofpHjn
+constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"656F6670486A6E";
 
 end core_version_pkg;
 
