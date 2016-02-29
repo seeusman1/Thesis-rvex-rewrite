38b7f807660b52b166a67ef9a2031e8c201d91a0
Anthony Brandon <a.a.c.brandon@tudelft.nl>
2016-02-29 17:57:24 +0100

    Some changes to python bindings.

--- config/templates/core_map.py
+++ config/templates/core_map.py
@@ -4,12 +4,43 @@ def raise_(ex):
 
 class Context:
 
+    def halt(self):
+        self._rvd.writeInt(self._DCR, 1, 0x09)
+        return
+
+    def reset(self):
+        self.halt()
+        #set general regs to 0
+        for reg in range(64):
+            self.CREG_GPREG[reg] = 0
+        #reset
+        self._rvd.writeInt(self._DCR, 1, 0x80)
+
+    def resume(self):
+        self._rvd.writeInt(self._DCR, 1, 0x0c)
+
+    def step(self):
+        self._rvd.writeInt(self._DCR, 1, 0x0a)
+
+    class GPREGS:
+
+        def __init__(self, c):
+            self._c = c
+
+        def __getitem__(self, index):
+            return self._c._rvd.readInt(self._c._CREG_GPREG + index*4, 4)
+
+        def __setitem__(self, index, value):
+            self._c._rvd.writeInt(self._c._CREG_GPREG + index*4, 4, value)
+
     def __init__(self, rvd, base_address, index):
         self._rvd = rvd
         self._CREG = base_address
         self._CUR_CONTEXT = index
 
         self._CREG_GPREG = self._CREG + 0x100 + (self._CUR_CONTEXT * 0x400)
+        self.CREG_GPREG = self.GPREGS(self)
+
         self._CREG_CTXT = self._CREG + 0x200 + (self._CUR_CONTEXT * 0x400)
 @CTXT_REGISTER_ADDR
         return
