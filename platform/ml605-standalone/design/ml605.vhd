bef8c8472019b14884a80b614cb21e08ddee1d83
Jeroen van Straten <jeroen.van.straten@gmail.com>
2016-04-10 00:08:29 +0200

    Ran into a (the?) cache bug on standalone-cached in a very reproducable way:
     - Synthesize the design with the CFG vector currently specified in ml605.vhd
     - make run-cache-test DYNAMIC=true-O1 BUNDLE_ALIGN=2 LANECONFIG=FBFBFBFB BUSLAT=20
     - qurt fails at high interrupt rates
    
    This ONLY happens with the cached version of the standalone platform, and it
    only seems to happen when reconfiguring without affecting the context qurt runs
    in (for example 0x1188 to 0x1180, qurt in context 1). It also only seems to
    happen at high interrupt rates, but that's probably just random chance.

--- platform/ml605-standalone/design/ml605.vhd
+++ platform/ml605-standalone/design/ml605.vhd
@@ -114,17 +114,17 @@ architecture Behavioral of ml605 is
 --=============================================================================
   
   -- Core and standalone system configuration WITHOUT cache.
-  constant CFG                  : rvex_sa_generic_config_type := rvex_sa_cfg(
-    core => rvex_cfg(
-      numLanesLog2              => 3,
-      numLaneGroupsLog2         => 2,
-      numContextsLog2           => 2,
-      traceEnable               => 1
-    ),
-    core_valid                  => true,
-    imemDepthLog2B              => 18, -- 256 kiB (0x00000..0x3FFFF)
-    dmemDepthLog2B              => 18
-  );
+--  constant CFG                  : rvex_sa_generic_config_type := rvex_sa_cfg(
+--    core => rvex_cfg(
+--      numLanesLog2              => 3,
+--      numLaneGroupsLog2         => 2,
+--      numContextsLog2           => 2,
+--      traceEnable               => 1
+--    ),
+--    core_valid                  => true,
+--    imemDepthLog2B              => 18, -- 256 kiB (0x00000..0x3FFFF)
+--    dmemDepthLog2B              => 18
+--  );
   
   -- Core and standalone system configuration WITH cache.
   --constant CFG                  : rvex_sa_generic_config_type := rvex_sa_cfg(
@@ -143,6 +143,47 @@ architecture Behavioral of ml605 is
 --    cache_config_valid          => true,
 --    dmemDepthLog2B              => 18 -- 256 kiB (0x00000..0x3FFFF)
 --  );
+  
+  constant CFG                  : rvex_sa_generic_config_type := (
+    core => (
+      numLanesLog2              => 3,
+      numLaneGroupsLog2         => 2,
+      numContextsLog2           => 2,
+      genBundleSizeLog2         => 3,
+      bundleAlignLog2           => 1,
+      multiplierLanes           => 2#11111111#,
+      memLaneRevIndex           => 1,
+      numBreakpoints            => 4,
+      forwarding                => true,
+      limmhFromNeighbor         => true,
+      limmhFromPreviousPair     => false,
+      reg63isLink               => false,
+      cregStartAddress          => X"FFFFFC00",
+      resetVectors              => (others => (others => '0')),
+      unifiedStall              => false,
+      gpRegImpl                 => RVEX_GPREG_IMPL_MEM,
+      traceEnable               => true,
+      perfCountSize             => 4,
+      cachePerfCountEnable      => true
+    ),
+    cache_enable                => true,
+    cache_config => (
+      instrCacheLinesLog2       => 7, -- 128*32 = 4 kiB per block, 16 kiB total
+      dataCacheLinesLog2        => 9  -- 512*4 = 2 kiB per block, 8 kiB total
+    ),
+    cache_bypassRange           => addrRange(match => "1-------------------------------"),
+    imemDepthLog2B              => 18,
+    dmemDepthLog2B              => 18, -- 256 kiB
+    traceDepthLog2B             => 13, -- 8 kiB
+    debugBusMap_imem            => addrRangeAndMap(match => "00-1----------------------------"),
+    debugBusMap_dmem            => addrRangeAndMap(match => "001-----------------------------"),
+    debugBusMap_rvex            => addrRangeAndMap(match => "1111----------------------------"),
+    debugBusMap_trace           => addrRangeAndMap(match => "1110----------------------------"),
+    debugBusMap_mutex           => false,
+    rvexDataMap_dmem            => addrRangeAndMap(match => "0-------------------------------"),
+    rvexDataMap_bus             => addrRangeAndMap(match => "1-------------------------------")
+  );
+  
   
   -- S-rec file specifying the initial contents for the memories.
   constant SREC_FILENAME        : string := "../examples/init.srec";
--- platform/ml605-standalone/examples/src/cache.c
+++ platform/ml605-standalone/examples/src/cache.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-// This program runs engine for 5 seconds and then runs jpeg for 5 seconds,
+// This program runs qurt for 5 seconds and then runs jpeg for 5 seconds,
 // assuming 10 MHz clock and then stops. It is written such that resetting
 // the rvex with GSR will restart execution. Don't use any other means of
 // resetting; if you do this CR_CNT won't be properly initialized, which is
@@ -12,22 +12,29 @@
 // words of the data memory):
 //   0x3FFFFFF0: configuration A
 //   0x3FFFFFF4: configuration B
-//   0x3FFFFFF8: 0 -> interrupt and program in cx0
-//               1 -> interrupt in cx0, program in cx1
-//   0x3FFFFFFC: wakeup configuration
+//   0x3FFFFFF8: -1     -> interrupt and program in cx0
+//               others -> interrupt in cx0, program in cx1
+//                         wakeup configuration A
+//   0x3FFFFFFC: wakeup configuration B
 //
 // Example A: switch between group 0 and groups 2:3 with a single hwctxt
 //   0x3FFFFFF0: 0x8880
 //   0x3FFFFFF4: 0x0088
-//   0x3FFFFFF8: 0
+//   0x3FFFFFF8: -1
 //   0x3FFFFFFC: don't care
 //
 // Example B: same as above, but use a different hwctxt for handling the
 // interrupt
 //   0x3FFFFFF0: 0x8881
 //   0x3FFFFFF4: 0x1188
-//   0x3FFFFFF8: 1
+//   0x3FFFFFF8: 0x8808
 //   0x3FFFFFFC: 0x8808
+//
+// Example C: same as above, but without actually interrupting the benchmarks
+//   0x3FFFFFF0: 0x8881
+//   0x3FFFFFF4: 0x1188
+//   0x3FFFFFF8: 0x8801
+//   0x3FFFFFFC: 0x1108
 //
 // The purpose of the program is to test cache + reconfiguration performance.
 //
@@ -36,11 +43,11 @@
 // The following registers are (ab)used for returning information:
 //   c0 CR_RSC1:  number of interrupts serviced
 //   c0 CR_RET:   0x00 -> finished
-//                0x01 -> execution error in engine
+//                0x01 -> execution error in qurt
 //                0x02 -> execution error in jpeg
 //                0xFF -> did not finish
-//   cx CR_SCRP1: number of engine runs
-//   cx CR_SCRP2: number of cycles spent on engine runs
+//   cx CR_SCRP1: number of qurt runs
+//   cx CR_SCRP2: number of cycles spent on qurt runs
 //   cx CR_SCRP3: number of jpeg runs
 //   cx CR_SCRP4: number of cycles spent on jpeg runs
 //
@@ -48,13 +55,15 @@
 //   0x3FFFFFF0..0x3FFFFFFB
 
 volatile long *config = 0x3FFFFFF0;
-#define CONFIG_A    config[0]
-#define CONFIG_B    config[1]
-#define CONFIG_X(i) config[i]
-#define USE_WAKE    config[2]
-#define WAKE_CFG    config[3]
+#define CONFIG_A      config[0]
+#define CONFIG_B      config[1]
+#define CONFIG_X(i)   config[i]
+#define USE_WAKE      (config[2] >= 0)
+#define WAKE_CFG_A    config[2]
+#define WAKE_CFG_B    config[3]
+#define WAKE_CFG_X(i) config[i+2]
 
-int run_engine_once(void);
+int run_qurt_once(void);
 int run_jpeg_once(void);
 void _stop(void);
 void interrupt(int id);
@@ -97,17 +106,17 @@ int main(void) {
 		
 	}
 	
-	// Run engine.
-	puts("\n\nEngine... ");
+	// Run qurt.
+	puts("\n\nQurt... ");
 	start = CR_CNT;
 	while (CR_CNT < 50000000) {
-		if (run_engine_once()) {
-			puts("\nEngine failed!\n");
+		if (run_qurt_once()) {
+			puts("\nQurt failed!\n");
 			state = ~1;
 			CR_RET = 1;
 			_stop();
 		}
-		puts("e");
+		puts("q");
 		CR_SCRP1++;
 	}
 	CR_SCRP2 = CR_CNT - start;
@@ -159,12 +168,14 @@ void wake_interrupt(void) {
 	// Record the number of interrupts serviced.
 	CR_RSC1++;
 	
-	// Enable the wakeup system.
-	CR_WCFG = WAKE_CFG;
-	CR_SAWC = 1;
-	
 	// Toggle configuration.
 	current_mode = !current_mode;
+	
+	// Enable the wakeup system.
+	CR_WCFG = WAKE_CFG_X(current_mode);
+	CR_SAWC = 1;
+	
+	// Reconfigure.
 	CR_CRR = CONFIG_X(current_mode);
 	
 	// Wait for interrupt by waiting until a wakeup is triggered.
