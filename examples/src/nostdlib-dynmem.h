00c0f202b6818b2b1b18cfd0de11966444e0a853
Jeroen van Straten <jeroen.van.straten@gmail.com>
2017-01-10 15:16:58 +0100

    Remove old generic examples directory.

--- examples/src/nostdlib-dynmem.h
+++
@@ -1,7 +0,0 @@
-
-// Very simplistic dynamic memory allocation.
-void *malloc(unsigned long size);
-void  free(void *ptr);
-void *calloc(unsigned long nmemb, unsigned long size);
-void *realloc(void *ptr, unsigned long size);
-
