cfa91fff9f1117a0e5ebabe30fc489103b94cd1b
Jeroen van Straten <jeroen.van.straten@gmail.com>
2017-01-10 14:12:20 +0100

    Removed combinatorial loop that snuck in while doing the power optimizations.

--- lib/rvex/core/core_version_pkg.vhd
+++ lib/rvex/core/core_version_pkg.vhd
@@ -56,8 +56,8 @@ package core_version_pkg is
 
 -- The value below is the version tag for the core, which is automatically
 -- generated based on the MD5 hash of the contents of the other source files.
--- ASCII tag = kHzw-eF
-constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"6B487A772D6546";
+-- ASCII tag = AsvHOdH
+constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"417376484F6448";
 
 end core_version_pkg;
 
