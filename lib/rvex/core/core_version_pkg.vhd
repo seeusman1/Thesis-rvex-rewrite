41cdcde731e3758d5e267ec1b933e5aeb4f6d06c
Jeroen van Straten <jeroen.van.straten@gmail.com>
2017-09-25 12:53:23 +0200

    Updated user manual

--- lib/rvex/core/core_version_pkg.vhd
+++ lib/rvex/core/core_version_pkg.vhd
@@ -56,8 +56,8 @@ package core_version_pkg is
 
 -- The value below is the version tag for the core, which is automatically
 -- generated based on the MD5 hash of the contents of the other source files.
--- ASCII tag = v3n41AY
-constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"76336E34314159";
+-- ASCII tag = ujA-LOP
+constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"756A412D4C4F50";
 
 end core_version_pkg;
 
