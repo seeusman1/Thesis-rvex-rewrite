8fe715432bee485ede86f0920e56f9b81cf5f6e4
Usman <m.u.saleem@student.tudelft.nl>
2018-03-27 17:22:06 +0200

    modified tmr_voter

--- lib/rvex/core/tmr_voter.vhd
+++ lib/rvex/core/tmr_voter.vhd
@@ -1,32 +1,28 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity tmr_voter is 
-port(
+--=============================================================================
+entity tmr_voter is
+--=============================================================================
 
-	input_1		: in std_logic;
-	input_2		: in std_logic;
-	input_3		: in std_logic;
-	output		: out std_logic
+	port(
 
-);
+		input_1		: in std_logic;
+		input_2		: in std_logic;
+		input_3		: in std_logic;
+		output		: out std_logic
+	);
 end tmr_voter;
 
 architecture behavior of tmr_voter is
 	
-signal and_1_2		: std_logic;
-signal and_2_3		: std_logic;
-signal and_1_3		: std_logic;
-signal rst			: std_logic;
-
+	signal and_1_2		: std_logic;
+  	signal and_2_3		: std_logic;
+  	signal and_1_3		: std_logic;
+  	signal rst			: std_logic;
 
 begin
 	
-	process (input_1,input_2,input_3) is 
-	begin
-
 	output	<= (input_1 and input_2) or (input_2 and input_3) or (input_1 and input_3);
-
-	end process;
 
 end behavior;
