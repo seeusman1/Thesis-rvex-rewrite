d42596757cd4420173c6fa1fdd921d4483a12905
Usman <m.u.saleem@student.tudelft.nl>
2018-05-07 19:50:00 +0200

    added missing files in ISE for synthesis

--- lib/rvex/core/core_version_pkg.vhd
+++ lib/rvex/core/core_version_pkg.vhd
@@ -56,8 +56,8 @@ package core_version_pkg is
 
 -- The value below is the version tag for the core, which is automatically
 -- generated based on the MD5 hash of the contents of the other source files.
--- ASCII tag = gwmvqzq
-constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"67776D76717A71";
+-- ASCII tag = vQykgha
+constant RVEX_CORE_TAG : std_logic_vector(55 downto 0) := X"7651796B676861";
 
 end core_version_pkg;
 
