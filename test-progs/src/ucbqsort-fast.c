ad6d375255184997407f6a58f7e83b3d852c2965
Usman <m.u.saleem@student.tudelft.nl>
2018-05-25 16:21:17 +0200

    modified ucbqsort-fast to request reconf 3 times... like ~fixit

--- test-progs/src/ucbqsort-fast.c
+++ test-progs/src/ucbqsort-fast.c
@@ -15,21 +15,22 @@ int compare(char *n1, char *n2)
 }
 int main()
 {
-	CR_CRR = 0x8309;
+	//CR_CRR = 0x8309;
 	//CR_CRR = 0x8999;
-	//CR_CRR = 0x0888;
+	CR_CRR = 0x0888;
 	////////////CR_CRR = 0x0001; 
 
 	int j;
 	QSORT((char *) SortArr, (int) 20, sizeof(unsigned char), compare);
-	CR_CRR = 0x8880;
+	//CR_CRR = 0x8880;
+	CR_CRR = 0x8309;
 	for (j = 0; j < 20; j++) {
 		if (SortArr[j] != SortedArr[j]) {
 			rvex_fail("ucbqsort-fast: failed\n");
 			return 1;
 		}
 	}
-	//CR_CRR = 0x8800;
+	CR_CRR = 0x0888;
 
 	rvex_succeed("ucbqsort-fast: success\n");
 
